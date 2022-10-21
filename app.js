5f22ae360a55f6d6ebe9ee27174eb5d0288a0dec
Ammar1201 <aamour1201@gmail.com>
2022-10-21 20:06:14 +0300

    added normalize.css and responsive site done

--- app.js
+++ app.js
@@ -21,56 +21,112 @@ const toolBarTools = document.querySelector('#toolBarTools');
 const inventorySlots = document.querySelectorAll('#inventory > div');
 
 const createWorld = () => {
-  for(let i = 0; i < WORLD_GRID_ROWS; i++) {
-    for(let j = 0; j < WORLD_GRID_COLUMNS; j++) {
-      const div = document.createElement('div');
-
-      //* add clouds
-      if(i ==2 || i == 3 || i == 4) {
-        if(j == 2 || j == 3 || j == 4 || j == 5) {
-          div.classList.add('cloud');
-        }
-      }
-
-      //* add leaves
-      if(i == 5 ) {
-        if(j == 14 || j == 15  || j == 24 || j == 25) {
-          div.classList.add('leaves');
-        }
-      }
-      if(i == 6 || i == 7 || i == 8) {
-        if(j == 13 || j == 14 || j == 15 || j == 16 || j == 23 || j == 24 || j == 25 || j == 26) {
-          div.classList.add('leaves');
-        }
-      }
-
-      //* add wood
-      if(i === 9 || i == 10 || i == 11 || i == 12) {
-        if(j == 14 || j == 15 || j == 24 || j == 25) {
-          div.classList.add('wood');
-        }
-
-        //* add stone
-        if(i == 11 || i == 12) {
-          if(j == 2 || j == 3 || j == 4 || j == 5 || j == 6 || j == 7) {
-            div.classList.add('stone');
+  if(window.innerWidth < 770) {
+    for(let i = 0; i < WORLD_GRID_ROWS; i++) {
+      for(let j = 0; j < 20; j++) {
+        const div = document.createElement('div');
+  
+        //* add clouds
+        if(i ==2 || i == 3 || i == 4) {
+          if(j == 2 || j == 3 || j == 4 || j == 5) {
+            div.classList.add('cloud');
           }
         }
-        if(i === 9 || i == 10) {
-          if(j == 3 || j == 4 || j == 5 || j == 6) {
-            div.classList.add('stone');
+  
+        //* add leaves
+        if(i == 5 ) {
+          if(j == 14 || j == 15  || j == 24 || j == 25) {
+            div.classList.add('leaves');
           }
         }
+        if(i == 6 || i == 7 || i == 8) {
+          if(j == 13 || j == 14 || j == 15 || j == 16 || j == 23 || j == 24 || j == 25 || j == 26) {
+            div.classList.add('leaves');
+          }
+        }
+  
+        //* add wood
+        if(i === 9 || i == 10 || i == 11 || i == 12) {
+          if(j == 14 || j == 15 || j == 24 || j == 25) {
+            div.classList.add('wood');
+          }
+  
+          //* add stone
+          if(i == 11 || i == 12) {
+            if(j == 2 || j == 3 || j == 4 || j == 5 || j == 6 || j == 7) {
+              div.classList.add('stone');
+            }
+          }
+          if(i === 9 || i == 10) {
+            if(j == 3 || j == 4 || j == 5 || j == 6) {
+              div.classList.add('stone');
+            }
+          }
+        }
+  
+        //* add grass & dirt
+        if(i == 13) {
+          div.setAttribute('class', 'grass');
+        }
+        if(i > 13) {
+          div.setAttribute('class', 'dirt');
+        }
+        world.appendChild(div);
       }
+    }
+  }
+  else {
+    for(let i = 0; i < WORLD_GRID_ROWS; i++) {
+      for(let j = 0; j < WORLD_GRID_COLUMNS; j++) {
+        const div = document.createElement('div');
 
-      //* add grass & dirt
-      if(i == 13) {
-        div.setAttribute('class', 'grass');
+        //* add clouds
+        if(i ==2 || i == 3 || i == 4) {
+          if(j == 2 || j == 3 || j == 4 || j == 5) {
+            div.classList.add('cloud');
+          }
+        }
+
+        //* add leaves
+        if(i == 5 ) {
+          if(j == 14 || j == 15  || j == 24 || j == 25) {
+            div.classList.add('leaves');
+          }
+        }
+        if(i == 6 || i == 7 || i == 8) {
+          if(j == 13 || j == 14 || j == 15 || j == 16 || j == 23 || j == 24 || j == 25 || j == 26) {
+            div.classList.add('leaves');
+          }
+        }
+
+        //* add wood
+        if(i === 9 || i == 10 || i == 11 || i == 12) {
+          if(j == 14 || j == 15 || j == 24 || j == 25) {
+            div.classList.add('wood');
+          }
+
+          //* add stone
+          if(i == 11 || i == 12) {
+            if(j == 2 || j == 3 || j == 4 || j == 5 || j == 6 || j == 7) {
+              div.classList.add('stone');
+            }
+          }
+          if(i === 9 || i == 10) {
+            if(j == 3 || j == 4 || j == 5 || j == 6) {
+              div.classList.add('stone');
+            }
+          }
+        }
+
+        //* add grass & dirt
+        if(i == 13) {
+          div.setAttribute('class', 'grass');
+        }
+        if(i > 13) {
+          div.setAttribute('class', 'dirt');
+        }
+        world.appendChild(div);
       }
-      if(i > 13) {
-        div.setAttribute('class', 'dirt');
-      }
-      world.appendChild(div);
     }
   }
 }
@@ -208,6 +264,13 @@ const placeBlock = (target) => {
 
 //! ---------------------EventListeners-------------------------------------------------------------------
 const StartEventListeners = () => {
+
+  startBtn.addEventListener('click', () => {
+    const game = document.querySelector('#game');
+    game.classList = 'displayAll';
+    const landingPage = document.querySelector('#landing-page');
+    landingPage.classList = 'displayNone';
+  });
 
   world.addEventListener('click', (event) => {
     const target = event.target;
